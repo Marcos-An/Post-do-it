c4d0222e3b5d067862d1180ad1bd66b5bf2a85c1
Marcos_ <loseonjr@gmail.com>
2020-11-25 07:53:51 -0400

    Card details to create the tasks

---
+++ src/components/card-details.vue
@@ -0,0 +1,17 @@
+<template>
+     <vs-dialog v-model="visible" v-on:close="handleVisible">
+        <template #header>
+            <h4 class="not-margin">
+              {{post.name}}
+            </h4>
+        </template>
+      </vs-dialog>
+</template>
+<script>
+export default {
+  props: ['visible', 'post', 'handleVisible']
+}
+</script>
+<style scoped>
+
+</style>
